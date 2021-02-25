3de736fc4d1b14b8fb0217334f9519f178593203
Valentina Zea <mavalezea@gmail.com>
2021-02-25 12:37:49 -0500

    Primer commit

---
+++ pasos.md
@@ -0,0 +1,9 @@
+# Funcion Cypher
+1. Recorrer la cadena y hacer console log a cada letra // for
+2. console log cada numero asscci de cada letra // charCodeAt
+ej. A 65
+3. console log  de el nuevo numero asscci (formula de michelle para obtener el nuevo numero asscci)
+ej. 68 -> ?
+4. console log de la nueva letra // fromCharCode
+ej. 68 -> C
+5. al final toca concatenar (juntar las nuevas letras a las que le hicimos console.log en una variable)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
+function saveName(){
+    var username = document.getElementById('username').value;
+    console.log(username);
+}
+
+function cambioPantallas(section){
+    var elemWelcome = document.getElementById('welcome');
+    var elemKey = document.getElementById('key');
+    console.log("Entró a cambio pantallas");
+
+    if(section == 'welcome'){
+        elemWelcome.style.display = 'display';
+        elemKey.style.display = 'none';
+    }
+    else if( section == 'key'){
+        elemWelcome.style.display = 'none';
+        elemKey.style.display = 'display';
+    }
+}
+
+let btnBienvenida = document.getElementById("btnBienvenida");
+btnBienvenida.addEventListener('click', saveName);
+
+// let btnClave = document.getElementById("btnClave");
+// btnClave.addEventListener('click', cambioPantallas);
+
+
+
+
+
+
 console.log(cipher);
